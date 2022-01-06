859acc92e6a336b68932d9e3ab47c3f1892b9422
Dorian Maliszewski <dmaliszewski@scaleway.com>
2022-01-06 16:46:29 +0100

    feat: handle when multiple errors, get first valid string

--- src/providers/ErrorContext/index.tsx
+++ src/providers/ErrorContext/index.tsx
@@ -33,20 +33,26 @@ const ErrorProvider = ({
       meta,
       ...additionalParams
     }: FormErrorFunctionParams & AnyObject) => {
-      if (meta?.error && errors[meta.error as keyof typeof errors]) {
-        const key = meta.error as keyof typeof errors
-        if (typeof errors[key] === 'function') {
-          return (errors[key] as (params: AnyObject) => string)({
-            allValues,
-            label,
-            meta,
-            name,
-            value,
-            ...additionalParams,
-          })
-        }
+      if (meta?.error && Array.isArray(meta.error)) {
+        return (
+          meta.error
+            .map(untypedKey => {
+              const key = untypedKey as keyof typeof errors
+              if (typeof errors[key] === 'function') {
+                return (errors[key] as (params: AnyObject) => string)({
+                  allValues,
+                  label,
+                  meta,
+                  name,
+                  value,
+                  ...additionalParams,
+                })
+              }
 
-        return errors[key]
+              return errors[key]
+            })
+            .filter(errorString => !!errorString)[0] ?? ''
+        )
       }
       // With a custom validate function the user may return a string directly
       if (meta?.error && typeof meta?.error === 'string') return meta?.error

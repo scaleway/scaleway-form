c4ceaa796e5428196a0fea6e6654359877827cb5
Emmanuel Chambon <echambon@scaleway.com>
2022-02-10 12:34:35 +0100

    feat: customizable variant

--- src/components/Submit/index.tsx
+++ src/components/Submit/index.tsx
@@ -1,21 +1,22 @@
 import { Button } from '@scaleway/ui'
-import React, { ReactNode } from 'react'
+import React, { ComponentProps, ReactNode } from 'react'
 import { useFormState } from 'react-final-form'
 
 type SubmitProps = {
   children?: ReactNode
   disabled?: boolean
   className?: string
+  variant?: ComponentProps<typeof Button>['variant']
 }
 
-const Submit = ({ children, disabled = false, className }: SubmitProps): JSX.Element => {
+const Submit = ({ children, disabled = false, variant = "success", className }: SubmitProps): JSX.Element => {
   const { invalid, submitting } = useFormState()
 
   return (
     <Button
       progress={submitting}
       disabled={disabled || submitting || invalid}
-      variant="success"
+      variant={variant}
       type="submit"
       className={className}
     >

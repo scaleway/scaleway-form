ab32ae0099ab2f5cccfafd2ed64b322cd7f8d287
Emmanuel Chambon <echambon@scaleway.com>
2022-02-10 12:26:16 +0100

    feat: allow Submit customization and fix form error cascade

--- src/components/Submit/index.tsx
+++ src/components/Submit/index.tsx
@@ -2,19 +2,22 @@ import { Button } from '@scaleway/ui'
 import React, { ReactNode } from 'react'
 import { useFormState } from 'react-final-form'
 
-export type SubmitProps = {
+type SubmitProps = {
   children?: ReactNode
+  disabled?: boolean
+  className?: string
 }
 
-const Submit = ({ children }: SubmitProps): JSX.Element => {
+const Submit = ({ children, disabled = false, className }: SubmitProps): JSX.Element => {
   const { invalid, submitting } = useFormState()
 
   return (
     <Button
       progress={submitting}
-      disabled={submitting || invalid}
+      disabled={disabled || submitting || invalid}
       variant="success"
       type="submit"
+      className={className}
     >
       {children}
     </Button>

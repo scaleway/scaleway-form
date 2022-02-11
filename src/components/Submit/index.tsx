9213fb6c972482f43bebe856cee806e153d9fe0d
Emmanuel Chambon <echambon@scaleway.com>
2022-02-11 15:39:43 +0100

    fix: allow to resubmit if submitError is not related to validation

--- src/components/Submit/index.tsx
+++ src/components/Submit/index.tsx
@@ -9,13 +9,25 @@ type SubmitProps = {
   variant?: ComponentProps<typeof Button>['variant']
 }
 
-const Submit = ({ children, disabled = false, variant = "success", className }: SubmitProps): JSX.Element => {
-  const { invalid, submitting } = useFormState()
+const Submit = ({
+  children,
+  disabled = false,
+  variant = 'success',
+  className,
+}: SubmitProps): JSX.Element => {
+  const { invalid, submitting, hasValidationErrors, dirtySinceLastSubmit } = useFormState({
+    subscription: {
+      dirtySinceLastSubmit: true,
+      hasValidationErrors: true,
+      invalid: true,
+      submitting: true,
+    },
+  })
 
   return (
     <Button
       progress={submitting}
-      disabled={disabled || submitting || invalid}
+      disabled={disabled || submitting || (invalid && hasValidationErrors && dirtySinceLastSubmit)}
       variant={variant}
       type="submit"
       className={className}

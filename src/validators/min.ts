423352951c2b2ddc2c73a5eff347d16614e6442f
Manu Chambon <echambon@scaleway.com>
2022-07-19 16:48:17 +0200

    fix(validators): min/max should check for equality (#406)

--- src/validators/min.ts
+++ src/validators/min.ts
@@ -2,7 +2,7 @@ import { ValidatorFn } from './types'
 
 const minValidator: ValidatorFn<number> = min => ({
   error: 'TOO_LOW',
-  validate: value => Number(value) > min,
+  validate: value => Number(value) >= min,
 })
 
 export default minValidator

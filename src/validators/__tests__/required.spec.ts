d5ef502375b0dbfdd8fa50776b6484f553a8e62d
Baptiste Arnaud <barnaud@ext.scaleway.com>
2022-08-30 14:44:37 +0200

    fix: ci tests

--- src/validators/__tests__/required.spec.ts
+++ src/validators/__tests__/required.spec.ts
@@ -10,11 +10,14 @@ describe('required validator', () => {
     expect(required(undefined).validate('test', {})).toBe(true)
     expect(required(undefined).validate(true, {})).toBe(true)
     expect(required(undefined).validate({}, {})).toBe(true)
+    expect(required(false).validate('', {})).toBe(true)
+    expect(required(false).validate(undefined, {})).toBe(true)
+    expect(required(false).validate(false, {})).toBe(true)
   })
 
   test('should failed', () => {
-    expect(required(undefined).validate('', {})).toBe(false)
-    expect(required(undefined).validate(undefined, {})).toBe(false)
-    expect(required(undefined).validate(false, {})).toBe(false)
+    expect(required(true).validate('', {})).toBe(false)
+    expect(required(true).validate(undefined, {})).toBe(false)
+    expect(required(true).validate(false, {})).toBe(false)
   })
 })

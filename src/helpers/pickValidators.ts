1a81455b51a986ba761022bd7eed3966c30a352e
Baptiste Arnaud <barnaud@ext.scaleway.com>
2022-08-30 14:16:25 +0200

    fix: require={false} should work

--- src/helpers/pickValidators.ts
+++ src/helpers/pickValidators.ts
@@ -5,11 +5,11 @@ import { ValidatorFn } from '../validators/types'
 const pickValidators = <InputValue = unknown>(args: ValidatorProps) =>
   Object.entries(args)
     .map(([key, value]) =>
-      value !== undefined
-        ? (validators as Record<string, ValidatorFn>)[
+      value === undefined || (key === 'required' && value === false)
+        ? undefined
+        : (validators as Record<string, ValidatorFn>)[
             key as keyof typeof validators
-          ]?.(value)
-        : undefined,
+          ]?.(value),
     )
     .filter(validator => !!validator) as ValidatorObject<InputValue>[]
 

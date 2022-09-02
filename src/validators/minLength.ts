597bb8440df9619d5d8d475b1e8b2a2776ba4546
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2022-09-02 16:26:09 +0200

    fix(validation): min and max length when value is undefined (#469)

--- src/validators/minLength.ts
+++ src/validators/minLength.ts
@@ -2,7 +2,7 @@ import { ValidatorFn } from './types'
 
 const minLengthValidator: ValidatorFn<string, number> = minLength => ({
   error: 'MIN_LENGTH',
-  validate: value => value?.length >= minLength,
+  validate: value => (value?.length ?? 0) >= minLength,
 })
 
 export default minLengthValidator
